ae407cc9c035f7156b5886b400aa6b1a6482e485
PiotrSm <franko102@interia.pl>
2020-11-07 10:53:20 +0100

    first commit of the project

---
+++ app/src/main/java/com/homeworkshop/notemvvm/MainActivity.java
@@ -0,0 +1,33 @@
+package com.homeworkshop.notemvvm;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.lifecycle.ViewModelProviders;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    private NoteViewModel noteViewModel;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        noteViewModel = ViewModelProviders.of(this).get(NoteViewModel.class); // to jest depracated
+        // nowy sposób inicjalizacji ViewModelu - konstruktorem
+        noteViewModel = new ViewModelProvider(this).get(NoteViewModel.class);
+        //ta metoda jest uruchamiana tylko jeżeli dostarczymy referencje do obecej activity
+        noteViewModel.getAllNotes().observe(this, new Observer<List<Note>>() {
+            @Override
+            public void onChanged(List<Note> notes) {
+                //update RecylcerViere
+                Toast.makeText(MainActivity.this, "onChanged", Toast.LENGTH_SHORT).show();//testowo
+            }
+        });
+    }
+}
